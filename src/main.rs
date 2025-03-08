2535d2ee6c991db99e6043d8efe89f2a116628a2
44David <david.soane@outlook.com>
2025-03-08 13:56:03 -0600

    Created and built app window

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
-fn main() {
-    println!("Hello, world!");
+use gtk::{prelude::*, ApplicationWindow};
+use gtk::{glib, Application};
+
+const APP_ID: &str = "org.gtk_rs.window";
+
+fn main() -> glib::ExitCode {
+    let app = Application::builder().application_id(APP_ID).build();
+    
+    app.connect_activate(build_ui);
+    
+    app.run()
+}
+
+fn build_ui(app: &Application) {
+    let window = ApplicationWindow::builder()
+        .application(app)
+        .title("gtk-rs app")
+        .build();
+    
+    window.present();
 }
